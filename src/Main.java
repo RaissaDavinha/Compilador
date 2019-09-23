2fed81c4847d73cf7b312570d46645df074413a2
Rafael-Fioramonte <fioramonterafael@gmail.com>
2019-09-23 11:50:50 -0300

    running, not fully done

--- src/Main.java
+++ src/Main.java
@@ -7,219 +7,331 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) throws IOException, LexicoException {
-		FileReader fileReader = new FileReader("lexico.txt");
+		FileReader fileReader = new FileReader("test1.txt");
 		BufferedReader reader = new BufferedReader(fileReader);
-		String readContent = reader.readLine();
+		String auxContent = null;
+		String fileContent = "";
 		List<Token> tokens = new ArrayList<Token>();
 		Token token = new Token();
-		int pos = 0;
-		int linha = 1;
-		char letra;
-		String id;
-		String num;
+		int fileContentIndex = 0;
+		char controlCharacter;
+		String tokenBuilder;
 		
 		
+		auxContent = reader.readLine();
+		while (auxContent != null) {
+			fileContent += auxContent;
+			fileContent += '\n';
+			auxContent = reader.readLine();
+		}
+
+		fileReader.close();
 		
+		System.out.print(fileContent);
 		
-		while (readContent != null) {
-			letra = readContent.charAt(pos);
-			while((letra == '{') || (letra == ' ') || (readContent != null)) {
-				if (letra == '{') {
-					while(letra != '}' && readContent != null) {
-						pos++;
-						letra = readContent.charAt(pos);
-					}
-					while((letra == ' ') && readContent != null) {
-						pos++;
-						letra = readContent.charAt(pos);
-					}
-				}
-				
-			}
+	
+		
+		while (fileContentIndex < fileContent.length()) {
 			
-			if (Character.isDigit(letra)) {
-				num = String.valueOf(letra);
-				pos++;
-				letra = readContent.charAt(pos);
-				
-				while(Character.isDigit(letra)) {
-					num = num + letra;
-					pos++;
-					letra = readContent.charAt(pos);
+			token = new Token();
+			
+			// le primeiro caracter
+			controlCharacter = fileContent.charAt(fileContentIndex);
+			fileContentIndex++;
+			
+			// le todos os espaços
+			if (controlCharacter == ' ') {
+				while (controlCharacter == ' ') {
+					if (fileContentIndex < fileContent.length()) {
+						controlCharacter = fileContent.charAt(fileContentIndex);
+						fileContentIndex++;
+					} else {
+						break;
+					}
 				}
-				
-				token.setSimbolo("snumero");
-				token.setLexema(num);
-				token.setLinha(linha);
-//				tokens.add(token);
-				
-			}else {
-				if (Character.isLetter(letra)) {
-					id = String.valueOf(letra);
-					pos++;
-					letra = readContent.charAt(pos);
-					
-					while(letra == '_' || Character.isDigit(letra) || Character.isLetter(letra)) {
-						id = id + letra;
-						pos++;
-						letra = readContent.charAt(pos);
-					}
-					token.setLexema(id);
-					token.setLinha(linha);
-					
-					switch (id) {
-					case "programa":
-						token.setSimbolo("sprograma");
-						break;
-						
-					case "se":
-						token.setSimbolo("sse");
-						break;
-						
-					case "entao":
-						token.setSimbolo("sentao");
-						break;
-						
-					case "ssenao":
-						token.setSimbolo("sprograma");
-						break;
-						
-					case "enquanto":
-						token.setSimbolo("senquanto");
-						break;
-						
-					case "faca":
-						token.setSimbolo("sfaca");
-						break;
-					
-					case "inicio":
-						token.setSimbolo("sinicio");
-						break;
-						
-					case "fim":
-						token.setSimbolo("sfim");
-						break;
-						
-					case "escreva":
-						token.setSimbolo("sescreva");
-						break;
-						
-					case "leia":
-						token.setSimbolo("sleia");
-						break;
-						
-					case "var":
-						token.setSimbolo("svar");
-						break;
-						
-					case "inteiro":
-						token.setSimbolo("sinteiro");
-						break;
-					
-					case "booleano":
-						token.setSimbolo("sbooleano");
-						break;
-					
-					case "verdadeiro":
-						token.setSimbolo("sverdadeiro");
-						break;
-						
-					case "falso":
-						token.setSimbolo("sfalso");
-						break;
-						
-					case "procedimento":
-						token.setSimbolo("sprocedimento");
-						break;
-						
-					case "funcao":
-						token.setSimbolo("sfuncao");
-						break;
-						
-					case "div":
-						token.setSimbolo("sdiv");
-						break;
-						
-					case "e":
-						token.setSimbolo("se");
-						break;
-						
-					default:
-						token.setSimbolo("sidentificador");
-					}
-					
-				}else {
-					if (letra == ':') {
-						if (readContent.charAt(pos + 1) == '=') {
-							num = ":=";
-							token.setLexema(num);
-							token.setSimbolo("satribuicao");
-							token.setLinha(linha);
-							pos++;
-//							pos++;
-						}else {
-							num = ":";
-							token.setLexema(num);
-							token.setSimbolo("sdoispontos");
-							token.setLinha(linha);
-//							pos++;
+			} else {
+				if (controlCharacter == '\n') {
+					while (controlCharacter == '\n') {
+						if (fileContentIndex < fileContent.length()) {
+							controlCharacter = fileContent.charAt(fileContentIndex);
+							fileContentIndex++;
+						} else {
+							break;
 						}
-					}else {
-						if (letra == '+' || letra == '-' || letra == '*') {
-//							Tratar Operador Aritmetico
-						}else {
-							if (letra == '<' || letra == '>' || letra == '=') {
-//								Trata Operador Relacional
-							}else {
-								if (letra == ';' || letra == ',' || letra == '(' || letra == ')' || letra == '.') {
-//									Trata Pontuacao
-									if(letra == ';') {
-										num = ";";
-										token.setLexema(num);
-										token.setSimbolo("sponto_virgula");
-										token.setLinha(linha);
+					}
+				} else {
+					// ler comentario
+					if (controlCharacter == '{') {
+						// procura caracter de fim de comentario
+						while (controlCharacter != '}') {
+							// caso for fim de arquivo e n tiver fim de comentario
+							if (fileContentIndex < fileContent.length()) {
+								controlCharacter = fileContent.charAt(fileContentIndex);
+								fileContentIndex++;
+							} else {
+								throw new LexicoException("Sem fim de comentário na linha: " + returnLineOfToken(fileContentIndex - 1, fileContent));
+							}
+							
+						}
+						
+					} else {
+						if (Character.isDigit(controlCharacter)) {
+							tokenBuilder = "";
+							while(Character.isDigit(controlCharacter)) {
+								tokenBuilder = tokenBuilder + controlCharacter;
+								if (fileContentIndex < fileContent.length()) {
+									controlCharacter = fileContent.charAt(fileContentIndex);
+									fileContentIndex++;
+								} else {
+									break;
+								}
+							}
+							
+							token.setSimbolo("snumero");
+							token.setLexema(tokenBuilder);
+							token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+							tokens.add(token);
+							
+						} else {
+							if (Character.isLetter(controlCharacter) && controlCharacter != ';' && controlCharacter != ':') {
+								tokenBuilder = "";
+								while((controlCharacter == '_' || Character.isDigit(controlCharacter) || Character.isLetter(controlCharacter)) && controlCharacter != ';' && controlCharacter != ':') {
+									tokenBuilder += controlCharacter;
+									if (fileContentIndex < fileContent.length()) {
+										controlCharacter = fileContent.charAt(fileContentIndex);
+										fileContentIndex++;
+									} else {
+										break;
 									}
-									if(letra == ',') {
-										num = ",";
-										token.setLexema(num);
-										token.setSimbolo("svirgula");
-										token.setLinha(linha);
+								}
+								
+								token.setLexema(tokenBuilder);
+								token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+								switch (tokenBuilder) {
+								case "programa":
+									token.setSimbolo("sprograma");
+									break;
+									
+								case "se":
+									token.setSimbolo("sse");
+									break;
+									
+								case "entao":
+									token.setSimbolo("sentao");
+									break;
+									
+								case "ssenao":
+									token.setSimbolo("sprograma");
+									break;
+									
+								case "enquanto":
+									token.setSimbolo("senquanto");
+									break;
+									
+								case "faca":
+									token.setSimbolo("sfaca");
+									break;
+								
+								case "inicio":
+									token.setSimbolo("sinicio");
+									break;
+									
+								case "fim":
+									token.setSimbolo("sfim");
+									break;
+									
+								case "escreva":
+									token.setSimbolo("sescreva");
+									break;
+									
+								case "leia":
+									token.setSimbolo("sleia");
+									break;
+									
+								case "var":
+									token.setSimbolo("svar");
+									break;
+									
+								case "inteiro":
+									token.setSimbolo("sinteiro");
+									break;
+								
+								case "booleano":
+									token.setSimbolo("sbooleano");
+									break;
+								
+								case "verdadeiro":
+									token.setSimbolo("sverdadeiro");
+									break;
+									
+								case "falso":
+									token.setSimbolo("sfalso");
+									break;
+									
+								case "procedimento":
+									token.setSimbolo("sprocedimento");
+									break;
+									
+								case "funcao":
+									token.setSimbolo("sfuncao");
+									break;
+									
+								case "div":
+									token.setSimbolo("sdiv");
+									break;
+									
+								case "e":
+									token.setSimbolo("se");
+									break;
+									
+								default:
+									token.setSimbolo("sidentificador");
+								}
+								tokens.add(token);
+							} else {
+								if (controlCharacter == ':') {
+									tokenBuilder = "";
+									if (fileContentIndex < fileContent.length()) {
+										if (fileContent.charAt(fileContentIndex) == '=') {
+											fileContentIndex++;
+											tokenBuilder = ":=";
+											token.setLexema(tokenBuilder);
+											token.setSimbolo("satribuicao");
+											token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+											tokens.add(token);
+										}else {
+											tokenBuilder = ":";
+											token.setLexema(tokenBuilder);
+											token.setSimbolo("sdoispontos");
+											token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+											tokens.add(token);
+										}
+									} else {
+										tokenBuilder = ":";
+										token.setLexema(tokenBuilder);
+										token.setSimbolo("sdoispontos");
+										token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+										tokens.add(token);
 									}
-									if(letra == '(') {
-										num = "(";
-										token.setLexema(num);
-										token.setSimbolo("sabre_parenteses");
-										token.setLinha(linha);
+								} else {
+									if (controlCharacter == '+' || controlCharacter == '-' || controlCharacter == '*') {
+			//							Tratar Operador Aritmetico
+										tokenBuilder = "";
+										if (controlCharacter == '+') {
+											token.setSimbolo("smais");
+										} else {
+											if (controlCharacter == '-') {
+												token.setSimbolo("smenos");
+											} else {
+												if (controlCharacter == '*') {
+													token.setSimbolo("smult");
+												}
+											}
+										}
+										tokenBuilder = Character.toString(controlCharacter);
+										token.setLexema(tokenBuilder);
+										token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+										tokens.add(token);
+									}else {
+										if (controlCharacter == '<' || controlCharacter == '>' || controlCharacter == '=') {
+			//								Trata Operador Relacional
+											tokenBuilder = "";
+											if (controlCharacter == '=') {
+												token.setSimbolo("sig");
+											} else {
+												if (controlCharacter == '>') {
+													if (fileContentIndex < fileContent.length()) {
+														if (fileContent.charAt(fileContentIndex) == '=') {
+															fileContentIndex++;
+															tokenBuilder += Character.toString(controlCharacter);
+															controlCharacter = fileContent.charAt(fileContentIndex);
+															token.setSimbolo("smaiorig");	
+														} else {
+															token.setSimbolo("smaior");
+														}
+													} else {
+														token.setSimbolo("smaior");
+													}
+												} else {
+													if (fileContentIndex < fileContent.length()) {
+														if (fileContent.charAt(fileContentIndex) == '=') {
+															fileContentIndex++;
+															tokenBuilder += Character.toString(controlCharacter);
+															controlCharacter = fileContent.charAt(fileContentIndex);
+															token.setSimbolo("smenorig");	
+														} else {
+															token.setSimbolo("smenor");
+														}
+													} else {
+														token.setSimbolo("smenor");
+													}
+												}
+											}
+											tokenBuilder += Character.toString(controlCharacter);
+											token.setLexema(tokenBuilder);
+											token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+											tokens.add(token);
+										}else {
+											if (controlCharacter == ';' || controlCharacter == ',' || controlCharacter == '(' || controlCharacter == ')' || controlCharacter == '.') {
+			//									Trata Pontuacao
+												if(controlCharacter == ';') {
+													tokenBuilder = ";";
+													token.setLexema(tokenBuilder);
+													token.setSimbolo("sponto_virgula");
+													token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+												}
+												if(controlCharacter == ',') {
+													tokenBuilder = ",";
+													token.setLexema(tokenBuilder);
+													token.setSimbolo("svirgula");
+													token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+												}
+												if(controlCharacter == '(') {
+													tokenBuilder = "(";
+													token.setLexema(tokenBuilder);
+													token.setSimbolo("sabre_parenteses");
+													token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+												}
+												if(controlCharacter == ')') {
+													tokenBuilder = ")";
+													token.setLexema(tokenBuilder);
+													token.setSimbolo("sfecha_parenteses");
+													token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+												}
+												if(controlCharacter == '.') {
+													tokenBuilder = ".";
+													token.setLexema(tokenBuilder);
+													token.setSimbolo("sponto");
+													token.setLinha(returnLineOfToken(fileContentIndex - 1, fileContent));
+												}
+												tokens.add(token);
+											} else {
+												throw new LexicoException("Erro Léxico na linha:" + returnLineOfToken(fileContentIndex - 1, fileContent));
+											}
+										}
 									}
-									if(letra == ')') {
-										num = ")";
-										token.setLexema(num);
-										token.setSimbolo("sfecha_parenteses");
-										token.setLinha(linha);
-									}
-									if(letra == '.') {
-										num = ".";
-										token.setLexema(num);
-										token.setSimbolo("sponto");
-										token.setLinha(linha);
-									}
-								}else {
-									throw new LexicoException("Erro Léxico na linha:" + linha);
 								}
 							}
 						}
 					}
 				}
 			}
-			
-			tokens.add(token);
-			readContent = reader.readLine();
-			pos = 0;
-			linha++;
 		}
-		
-		fileReader.close();
-
+			
+		for (int i = 0; i < tokens.size(); i++) {
+			System.out.print("<" + tokens.get(i).getSimbolo() + ">");
+		}
 	}
 
+	
+	static int returnLineOfToken(int fileContentIndex, String fileContent) {
+		int lineNumber;
+		int i;
+		for (lineNumber = 0, i = 0; i < fileContentIndex; i++) {
+			if (fileContent.charAt(i) == '\n') {
+				lineNumber++;
+			}
+		}
+		return lineNumber + 1;
+	}
 }
